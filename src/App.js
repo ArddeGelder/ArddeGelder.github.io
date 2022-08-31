5421cff1762ad24779f8675d21dde9ecaacf825b
ArddeGelder <arddegelder@gmail.com>
2022-08-31 20:55:29 +0200

    Add formating to tic-tac-toe board

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import logo from './logo.svg';
-import './App.css';
+
 import Timer from './Timer';
 import React from 'react';
 import Game from './TicTacToe/Game';
+import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />        
+        <img src={logo} className="App-logo" alt="logo" />
         <Game />
         <Timer />
       </header>
